bac589a3c73a063e6db4a566e9dcc9e71c885e28
matanki.saito <matanki.saito@gmail.com>
2019-09-20 02:21:18 +0900

    追加  - バージョン読み取りコードを追加  - デバッグ用コードを追加

---
+++ Plugin64/debug.cpp
@@ -0,0 +1,35 @@
+﻿#include "pch.h"
+#include "plugin_64.h"
+
+namespace Debug {
+
+	extern "C" {
+		void debugProc1();
+		uintptr_t debugProc1ReturnAddress;
+	}
+
+	errno_t debugProc1Injector(RunOptions options) {
+		switch (options.version) {
+		case v1_29_0:
+			// hook main thread head
+			// push rdi; push r12; ...
+			BytePattern::temp_instance().find_pattern("40 57 41 54 41 55 41 56 41 57 B8 F0 17 00 00");
+			if (BytePattern::temp_instance().has_size(1, "debug")) {
+				uintptr_t address = BytePattern::temp_instance().get_first().address();
+
+				debugProc1ReturnAddress = address + 15;
+
+				Injector::MakeJMP(address, debugProc1, true);
+			}
+			else return 1;
+			return NOERROR;
+		}
+		return 1;
+	}
+
+	errno_t Init(RunOptions options) {
+		errno_t result = 0;
+		result |= debugProc1Injector(options);
+		return result;
+	}
+}
---
+++ Plugin64/debug_asm.asm
@@ -0,0 +1,20 @@
+EXTERN debugProc1ReturnAddress: QWORD
+
+.CODE
+debugProc1 proc
+	mov rax, gs:[30h];
+	mov rax, [rax+60h];
+
+	mov byte ptr [rax+2],0;
+
+	push rdi;
+	push r12;
+	push r13;
+	push r14;
+	push r15;
+	mov eax, 17F0h; // ‚±‚±‚Å“h‚è‚Â‚Ô‚µ‚Ä‚¢‚é‚Ì‚Åã‚Årax‚ğg‚¦‚é
+
+	push debugProc1ReturnAddress;
+	ret;
+debugProc1 endp
+END
