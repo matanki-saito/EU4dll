6b8188fda034edef4ebb33629c64b4014db92fef
matanki.saito <matanki.saito@gmail.com>
2019-09-21 14:26:06 +0900

    修正：バージョン対応とヒープクリア処理の修正
     - ヒープクリアするためにdwFlagを0x8にした
     - 既存のコードを1.29.1.0に対応させた

--- Plugin64/font.cpp
+++ Plugin64/font.cpp
@@ -6,21 +6,27 @@ namespace Font {
 	extern "C" {
 		void fontBufferHeapZeroClear();
 		uintptr_t fontBufferHeapZeroClearReturnAddress;
-		uintptr_t heapAllocAddress;
+		uintptr_t fontBufferHeapZeroClearHeepAllocJmpAddress;
+		uintptr_t fontBufferHeapZeroClearHeapJmpAddress;
 	}
 
 	errno_t fontBufferHeapZeroClearInjector(RunOptions options) {
 		switch (options.version) {
-		case v1_29_0:
-			BytePattern::temp_instance().find_pattern("48 8B 0D 73 A8 A6 00");
+		case v1_29_0_0:
+		case v1_29_1_0:
+			// mov rcx,cs:hHeap
+			BytePattern::temp_instance().find_pattern("48 8B 0D ? ? ? ? 4C 8B C3 33 D2");
 			if (BytePattern::temp_instance().has_size(1, "Font buffer heap zero clear")) {
-				//heapAllocAddress = HeapAlloc;
+				uintptr_t address = BytePattern::temp_instance().get_first().address();
+				
+				// mov rcx, {cs:hHeap}
+				fontBufferHeapZeroClearHeapJmpAddress = Injector::GetBranchDestination(address + 0x0).as_int();
+				// call {{cs:HeapAlloc}}
+				fontBufferHeapZeroClearHeepAllocJmpAddress = Injector::GetBranchDestination(address + 0xC).as_int();
+				// jz short loc_xxxxx
+				fontBufferHeapZeroClearReturnAddress = address + 0x15;
 
-				//uintptr_t address = BytePattern::temp_instance().get_first().address();
-
-				//fontBufferHeapZeroClearReturnAddress = address + 15;
-
-				//Injector::MakeJMP(address, fontBufferHeapZeroClear, true);
+				Injector::MakeJMP(address, fontBufferHeapZeroClear, true);
 			}
 			else return 1;
 			return NOERROR;
@@ -31,7 +37,8 @@ namespace Font {
 
 	errno_t fontBufferExpansionInjector(RunOptions options) {
 		switch (options.version) {
-		case v1_29_0:
+		case v1_29_0_0:
+		case v1_29_1_0:
 			BytePattern::temp_instance().find_pattern("B9 68 3D 00 00");
 			if (BytePattern::temp_instance().has_size(1, "Font buffer expansion")) {
 				// mov ecx, 3D68h
@@ -46,7 +53,8 @@ namespace Font {
 
 	errno_t fontSizeLimitInjector(RunOptions options) {
 		switch (options.version) {
-		case v1_29_0:
+		case v1_29_0_0:
+		case v1_29_1_0:
 			BytePattern::temp_instance().find_pattern("41 81 FE 00 00 00 01");
 			if (BytePattern::temp_instance().has_size(1, "Font size limit")) {
 				// cmp r14d, 1000000h
@@ -67,7 +75,7 @@ namespace Font {
 		BytePattern::LoggingInfo("font etc fix");
 
 		/* ヒープゼロフラグの修正 */
-		//result |= heepZero_hook(options);
+		result |= fontBufferHeapZeroClearInjector(options);
 		/* フォントバッファクリア */
 		//result |= bufferClear_hook(options);
 		/* フォントバッファ拡張 */
